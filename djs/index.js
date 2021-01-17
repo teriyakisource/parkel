5625c7296212c2427217317c11e8c79e034d618c
teriyakisource <teriyakivass@gmail.com>
2021-01-17 23:28:05 +0000

    Initial commit

---
+++ djs/index.js
@@ -0,0 +1,1 @@
+const djs = require('discord.js')
---
+++ mng/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    args := os.args
+
+    if len(args) == 1 {
+        //check args
+        // new contanier:
+            //allocate less rescources to all containers
+        // deleted container:
+            //allocate more rescources to all containers
+
+        //calculate based off machine specifications
+
+        
+    }
+sl
+}
