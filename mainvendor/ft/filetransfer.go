f22b9de6aea6ca9dc56d55db77fb5baa15a1444f
teriyakisource <teriyakivass@gmail.com>
2021-01-18 10:36:03 +0000

    further commits

---
+++ mainvendor/ft/filetransfer.go
@@ -0,0 +1,17 @@
+package filetransfer
+
+import ( 
+    "fmt"
+)
+
+
+//this can be approached in a couple of ways:
+//  git
+//  some other file transfer magic
+//  git is more appropriate, yet more fiddly
+//  yet, would be a good idea to run a git server in the backend that the clients remotes to
+
+
+func main() {
+
+}
--- mng/main.go
+++ mng/main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+    "os/exec"
     "fmt"
-    "os"
 )
+
+
+type container struct {
+    epoch string,
+    uid string,
+}
 
 func main() {
     args := os.args
 
     if len(args) == 1 {
+        switch args[0] {
+            case "create":
+                on_create_container()
+        }
+    }
+
+    
         //check args
         // new contanier:
             //allocate less rescources to all containers
         // deleted container:
             //allocate more rescources to all containers
 
-        //calculate based off machine specifications
+        //calculate based off machine specification
+}
 
-        
-    }
-sl
+func on_create_container(tag string) bool {
+    stdout, err := exec.command("lxc list")
+    rescources_in_use := 
 }
