04b6222270b2664d9f9b03dc6fea91f130f2a99a
Vassilis <vassilisnin@gmail.com>
2021-01-18 19:36:04 +0000

    rust init for memory calc

--- lxc/launch.sh
+++ lxc/launch.sh
@@ -1,4 +1,6 @@
 lxc launch ubuntu:20.04 $1
 lxc exec $1 bash
     adduser runner    
+    sudo apt update
+    sudo apt install nodejs python3 neofetch 
 exit
---
+++ sfc/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    //add rescource checker
+}
